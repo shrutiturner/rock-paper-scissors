7c9a2e78626a98bb13b99d50d75b2b2a216f97d6
Shruti Turner <shrutiturner27@gmail.com>
2022-05-13 11:23:39 +0100

    Adds function get_winner to manual_rps.py.
    New function determines the winner between the computer and user.

--- manual_rps.py
+++ manual_rps.py
@@ -4,9 +4,9 @@ def get_computer_choice():
     """Generates the computer choice of rock, paper or scissors using random module."""
     
     choice_list = ['rock', 'paper', 'scissors']
-    comp_choice = random.choice(choice_list)
+    computer_choice = random.choice(choice_list)
 
-    return comp_choice
+    return computer_choice
 
 def get_user_choice():
     """Asks the user for a text input of their rock, paper, scissors choice and 
@@ -22,5 +22,23 @@ def get_user_choice():
         if user_choice.lower() in valid_choices:
             return user_choice
 
-        print("Invalid input, please provide one of the specified option ")
+        print("Invalid input, please provide one of the specified option.")
+
+
+def get_winner(computer_choice, user_choice):
+    """Takes two arguments, computer_choice and user_choice, and determined the winner of the rock, paper, scissors game."""
     
+    if computer_choice == user_choice:
+        return('Draw')
+    elif computer_choice == 'rock':
+        if user_choice == 'scissors':
+            return('Computer wins!')
+        return('User wins!')
+    elif computer_choice == 'paper':
+        if user_choice == 'rock':
+            return('Computer wins!')
+        return('User wins!')
+    elif computer_choice == 'scissors':
+        if user_choice == 'paper':
+            return('Computer wins!')
+        return('User wins!')
